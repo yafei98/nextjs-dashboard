6fc897cee6ce70ac83d53d1cbe8af63aa2baa0b5
polastar <1525502030@qq.com>
2024-08-22 14:17:43 +0800

    [0817]revert --- To chapter 9 - Steaming commit --Invoices

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -32,16 +32,11 @@ export type Revenue = {
 };
 
 export type LatestInvoice = {
-  // id: string;
-  // name: string;
-  // image_url: string;
-  // email: string;
-  // amount: string;
   id: string;
-  customer_id: string;
+  name: string;
+  image_url: string;
+  email: string;
   amount: string;
-  status: string;
-  date: string;
 };
 
 // The database returns a number for amount, but we later format it to a string with the formatCurrency function
