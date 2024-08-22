e560906eb189818da66b31afba19b7dd18ccf34d
polastar <1525502030@qq.com>
2024-08-22 14:12:40 +0800

    [0817]To chapter 11 - 4. Updating the table successed(without modifying anything)!!!!!

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useDebouncedCallback } from 'use-debounce';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
@@ -8,7 +9,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
 
     if (term) {
@@ -18,7 +21,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     console.log(params.toString());
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
