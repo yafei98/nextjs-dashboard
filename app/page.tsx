cb5629970b743fbbf01558c6f1d6cef8d209b391
Mira <lyf1998919@outlook.com>
2025-04-13 11:23:17 +0100

    [0314]Fix TS

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <p>yafei.dev@gmail.com</p>
       <div className={styles.shape} />
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
